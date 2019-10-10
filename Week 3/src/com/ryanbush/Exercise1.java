460134c456e5047907e70780bad16d1081438f9e
Ryan Bush <rpbush96@gmail.com>
2019-10-10 19:53:52 +0100

    Added Week 3 Exercise 1

---
+++ Week 3/src/com/ryanbush/Exercise1.java
@@ -0,0 +1,47 @@
+package com.ryanbush;
+
+public class Exercise1 {
+    public static void main(String args []) throws Exception {
+
+        MyThread6.count = 0 ;
+
+        MyThread6 thread1 = new MyThread6() ;
+        thread1.name = "A" ;
+
+        MyThread6 thread2 = new MyThread6() ;
+        thread2.name = "B" ;
+
+        thread1.start() ;
+        thread2.start() ;
+
+        thread2.join() ;
+        thread1.join() ;
+
+        System.out.println("MyThread6.count = " + MyThread6.count) ;
+    }
+}
+
+class MyThread6 extends Thread {
+    volatile static int count ;
+    String name ;
+    public void run() {
+        for(int i = 0 ; i < 10 ; i++) {
+            delay() ;
+            int x = count ;
+            System.out.println("Thread " + name + " read " + x) ;
+            delay() ;
+            count = x + 1 ;
+            System.out.println("Thread " + name + " wrote " + (x + 1)) ;
+        }
+    }
+
+    void delay() {
+
+        int delay = (int) (1000 * Math.random()) ;
+        try {
+            Thread.sleep(delay) ;
+        }
+        catch(Exception e) {
+        }
+    }
+}
